65bf807d255e365ea1e445b35f714dbdeed27749
Andrey Akinshin <andrey.akinshin@gmail.com>
2020-01-12 20:23:05 +0300

    CpuLoad

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Security.Cryptography;
 
 namespace PerfProber
 {
@@ -6,7 +8,42 @@ namespace PerfProber
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var runner = new Runner();
+            runner.CpuLoad(1); // Warmup
+
+            var sw = Stopwatch.StartNew();
+            runner.CpuLoad(100000);
+            sw.Stop();
+            Console.WriteLine("Elapsed: {0:0.000}", sw.Elapsed.TotalSeconds);
+        }
+    }
+
+    class Runner
+    {
+        private SHA256 sha256 = SHA256.Create();
+        private byte[] data;
+        public const int N = 100000;
+
+        public Runner()
+        {
+            data = new byte[N];
+            new Random(42).NextBytes(data);
+        }
+
+        public void Setup()
+        {
+            
+        }
+        
+        public int CpuLoad(int m)
+        {
+            var res = 0;
+            for (int i = 0; i < m; i++)
+            {
+                sha256.ComputeHash(data);
+                res += data[0];
+            }
+            return res;
         }
     }
 }
